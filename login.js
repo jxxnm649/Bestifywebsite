202e2876d4d968de286bc874b85bea26d650f97c
jxxnm649 <umeshaiaiai@gmail.com>
2026-07-27 22:45:13 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,25 @@
+import { auth } from "./firebase.js";
+
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+        await signInWithEmailAndPassword(auth, email, password);
+
+        alert("Login Successful!");
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+});
