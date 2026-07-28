7aa80c1656ce40ab13c88eaca468d248b74286af
jxxnm649 <umeshaiaiai@gmail.com>
2026-07-28 20:14:05 +0530

    Implement product loading from Firestore
    Load products from Firestore and display them on the page.

--- products.js
+++ products.js
@@ -1,1 +1,30 @@
-console.log("Products page loaded");
+import { db } from "./firebase.js";
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const productsDiv = document.getElementById("products");
+
+async function loadProducts() {
+
+    const querySnapshot = await getDocs(collection(db, "products"));
+
+    querySnapshot.forEach((doc) => {
+
+        const product = doc.data();
+
+        productsDiv.innerHTML += `
+            <div style="border:1px solid #ccc;padding:15px;margin:10px;">
+                <h2>${product.productName}</h2>
+                <p>₹${product.price}</p>
+                <p>${product.description}</p>
+            </div>
+        `;
+
+    });
+
+}
+
+loadProducts();
