fc93c0a507ae12e11916952d94683d73941d383e
jxxnm649 <umeshaiaiai@gmail.com>
2026-07-28 18:35:06 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,8 +1,10 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 
 import { onAuthStateChanged }
 from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
+import { doc, getDoc }
+from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 onAuthStateChanged(auth, (user) => {
     if (!user) {
         window.location.href = "login.html";
