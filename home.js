d9e756cf549ce309d88451f797e66d5be357b092
jxxnm649 <umeshaiaiai@gmail.com>
2026-07-28 18:36:25 +0530

    Update home.js

--- home.js
+++ home.js
@@ -5,8 +5,19 @@ from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
 import { doc, getDoc }
 from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
+
     if (!user) {
         window.location.href = "login.html";
+        return;
     }
+
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        document.getElementById("welcome").innerText =
+            "Welcome " + docSnap.data().name + " 👋";
+    }
+
 });
