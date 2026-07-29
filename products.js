f65995429d0242ec99acee3e00a97316a90103f2
jxxnm649 <umeshaiaiai@gmail.com>
2026-07-29 16:10:04 +0530

    Update product image source to a placeholder URL

--- products.js
+++ products.js
@@ -21,7 +21,7 @@ async function loadProducts() {
 
       productsDiv.innerHTML += `
 <div style="border:1px solid #ccc;padding:15px;margin:10px;border-radius:10px;">
-    <img src="${product.image}" width="150">
+    <img src="${https://picsum.photos/300}" width="150">
     <h2>${product.productName}</h2>
     <p>₹${product.price}</p>
     <p>${product.description}</p>
