b999ba05a0b7d36b0daa5d8dd70aca241546d6c6
jxxnm649 <umeshaiaiai@gmail.com>
2026-07-31 18:09:16 +0530

    Create checkout.js

---
+++ checkout.js
@@ -0,0 +1,80 @@
+import { auth, db } from "./firebase.js";
+
+import { onAuthStateChanged }
+from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc
+}
+from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const form = document.getElementById("checkoutForm");
+
+let currentUser = null;
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  currentUser = user;
+});
+
+form.addEventListener("submit", async (e) => {
+
+  e.preventDefault();
+
+  const customerName = document.getElementById("customerName").value;
+  const mobile = document.getElementById("mobile").value;
+  const address = document.getElementById("address").value;
+
+  try {
+
+    const cartSnapshot = await getDocs(
+      collection(db, "users", currentUser.uid, "cart")
+    );
+
+    if (cartSnapshot.empty) {
+      alert("Your Cart is Empty");
+      return;
+    }
+
+    const products = [];
+
+    cartSnapshot.forEach((docSnap) => {
+      products.push(docSnap.data());
+    });
+
+    await addDoc(collection(db, "orders"), {
+
+      userId: currentUser.uid,
+      customerName,
+      mobile,
+      address,
+      products,
+      createdAt: new Date()
+
+    });
+
+    for (const docSnap of cartSnapshot.docs) {
+      await deleteDoc(
+        doc(db, "users", currentUser.uid, "cart", docSnap.id)
+      );
+    }
+
+    alert("Order Placed Successfully 🎉");
+
+    window.location.href = "cart.html";
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+});
