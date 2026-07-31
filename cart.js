06136c982066a22e65299b3e1400c0c097b14480
jxxnm649 <umeshaiaiai@gmail.com>
2026-07-31 17:29:24 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,19 +1,15 @@
 import { auth, db } from "./firebase.js";
 
-import { onAuthStateChanged }
-from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
 import {
   collection,
   getDocs,
   doc,
   deleteDoc
-}
-from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 const cartDiv = document.getElementById("cartItems");
-
-alert("Cart JS Loaded");
 
 onAuthStateChanged(auth, async (user) => {
 
@@ -42,56 +38,48 @@ onAuthStateChanged(auth, async (user) => {
 
       cartDiv.innerHTML += `
         <div class="card">
-
           <img src="${product.image}" alt="${product.productName}">
-
           <div class="card-content">
-
             <h2>${product.productName}</h2>
-
             <p class="price">₹${product.price}</p>
-
             <p>${product.description}</p>
-
             <button onclick="removeFromCart('${docSnap.id}')">
               Remove
             </button>
-
           </div>
-
         </div>
       `;
+
     });
 
   } catch (error) {
-    alert(error.message);
     console.log(error);
+    alert(error.message);
   }
 
 });
 
 window.removeFromCart = async function(id) {
 
+  const user = auth.currentUser;
+
+  if (!user) {
+    alert("Please Login");
+    return;
+  }
+
   try {
-
-    const user = auth.currentUser;
-
-    if (!user) {
-      alert("Please Login");
-      return;
-    }
 
     await deleteDoc(
       doc(db, "users", user.uid, "cart", id)
     );
 
     alert("Removed Successfully ✅");
-
     location.reload();
 
   } catch (error) {
-    alert(error.message);
     console.log(error);
+    alert(error.message);
   }
 
 };
