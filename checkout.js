d911316350a18ba87ba3f4710967c8a5d1407de0
jxxnm649 <umeshaiaiai@gmail.com>
2026-08-03 01:42:15 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -28,7 +28,55 @@ onAuthStateChanged(auth, (user) => {
 form.addEventListener("submit", async (e) => {
 
   e.preventDefault();
+  const totalAmount = products.reduce(
+  (sum, item) => sum + Number(item.price),
+  0
+);
 
+const options = {
+  key: "rzp_test_TL1OXROVimUJpK",
+  amount: totalAmount * 100, // paise
+  currency: "INR",
+  name: "Bestify Store",
+  description: "Product Purchase",
+
+  handler: async function (response) {
+
+    await addDoc(collection(db, "orders"), {
+
+      userId: currentUser.uid,
+      customerName,
+      mobile,
+      address,
+      products,
+      total: totalAmount,
+      paymentId: response.razorpay_payment_id,
+      status: "Paid",
+      createdAt: new Date()
+
+    });
+
+    for (const docSnap of cartSnapshot.docs) {
+      await deleteDoc(
+        doc(db, "users", currentUser.uid, "cart", docSnap.id)
+      );
+    }
+
+    alert("Payment Successful ✅");
+
+    window.location.href = "orders.html";
+
+  },
+
+  theme: {
+    color: "#3399cc"
+  }
+};
+
+const rzp = new Razorpay(options);
+rzp.open();
+
+return;
   const customerName = document.getElementById("customerName").value;
   const mobile = document.getElementById("mobile").value;
   const address = document.getElementById("address").value;
