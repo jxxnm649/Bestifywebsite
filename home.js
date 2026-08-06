b87fecee1e41da5c55dae504cf32ae02a354dbb8
jxxnm649 <umeshaiaiai@gmail.com>
2026-08-06 21:13:33 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,23 +1,52 @@
 import { auth, db } from "./firebase.js";
 
-import { onAuthStateChanged }
-from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
-import { doc, getDoc }
-from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const welcome = document.getElementById("welcome");
+
 onAuthStateChanged(auth, async (user) => {
 
-    if (!user) {
-        window.location.href = "login.html";
-        return;
-    }
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  try {
 
     const docRef = doc(db, "users", user.uid);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-        document.getElementById("welcome").innerText =
-            "Welcome " + docSnap.data().name + " 👋";
+
+      const data = docSnap.data();
+
+      welcome.innerHTML = `
+        👋 Welcome <b>${data.name}</b>
+      `;
+
+    } else {
+
+      welcome.innerHTML = `
+        👋 Welcome ${user.email}
+      `;
+
     }
+
+  } catch (error) {
+
+    console.log(error);
+
+    welcome.innerHTML = `
+      👋 Welcome ${user.email}
+    `;
+
+  }
 
 });
